527c840935ba85f6b362f819e0511ac0d0ca7c14
Varga Zsolt Gergo <varga.zsolt.gergo@gmail.com>
2023-02-01 11:52:58 +0100

    add validation rules to swipe request

---
+++ app/Http/Requests/SwipeRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Swipe;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class SwipeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => [
+                'required',
+                'integer',
+                'exists:users,id',
+                Rule::unique('swipes')
+                    ->where('swiped_user_id', $this->swiped_user_id)
+            ],
+            'swiped_user_id' => [
+                'required',
+                'integer',
+                'exists:users,id',
+            ],
+            'action' => 'required|in:' . implode(',', Swipe::ALLOWED_ACTIONS)
+        ];
+    }
+}
